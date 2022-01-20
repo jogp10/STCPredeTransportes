98ac3eeeb4ee176a768f999c34cb16be8f24539c
jogp10 <jogp10@gmail.com>
2022-01-20 21:36:25 +0000

    Added Walk Edges

--- develop/graph/graph.cpp
+++ develop/graph/graph.cpp
@@ -7,7 +7,7 @@ Graph::Graph(int num, bool dir) : n(num), hasDir(dir), nodes(num+1) {
 }
 
 // Add edge from source to destination with a certain weight
-void Graph::addEdge(int src, int dest, int weight, string line) {
+void Graph::addEdge(int src, int dest, double weight, string line) {
     if (src<1 || src>n || dest<1 || dest>n) return;
     nodes[src].adj.push_back({dest, weight, line});
 }
@@ -25,19 +25,20 @@ map<int, pair<double, double>> Graph::getNodes(){
 // 1) Algoritmo de Dijkstra e caminhos mais curtos
 // ----------------------------------------------------------
 void Graph::dijkstra(int s) {
+    createWalkEdges();
+
     for(Node &n: nodes){
         n.dist=INT16_MAX;
         n.visited=false;
     }
 
-    nodes[0].visited=true;
     nodes[s].dist=0;
-    nodes[s].pred = s;
+    nodes[s].pred =s;
     int u = s;
 
     bool allVis = false;
     while(!allVis){
-        int min_dist = INT16_MAX;
+        double min_dist = INT16_MAX;
         for(int i=0; i<nodes.size(); ++i){
             if(nodes[i].dist<=min_dist && !nodes[i].visited){
                 u=i;
@@ -68,7 +69,7 @@ void Graph::dijkstra(int s) {
 // ..............................
 // a) Distância entre dois nós
 // TODO
-int Graph::dijkstra_distance(int a, int b) {
+double Graph::dijkstra_distance(int a, int b) {
     dijkstra(a);
     if(nodes[b].dist==INT16_MAX) return -1;
     return nodes[b].dist;
@@ -92,7 +93,7 @@ list<int> Graph::dijkstra_path(int a, int b) {
 }
 
 
-double Graph::getDistance(Graph::Node n1, Graph::Node n2) {
+double Graph::getDistance(const Graph::Node& n1, Graph::Node n2) {
     const double PI = 3.141592653589793238463;
 
     auto lat1 = n1.latitude, lat2 = n2.latitude;
@@ -110,6 +111,28 @@ double Graph::getDistance(Graph::Node n1, Graph::Node n2) {
              sin(dLon/2) * sin(dLon/2) * cos(lat1) * cos(lat2);
     auto c = 2 * atan2(sqrt(a), sqrt(1-a));
     return earthRadiusKM * c;
+}
+
+/**
+ * Create edges to wich you can walk, without need to take transport
+ * This type of edge has in maximum weight 2, because the distance beetween 2 points is 1 km.
+ * We duplicated the default weight to this type of edge
+ */
+void Graph::createWalkEdges() {
+    for(int i=0; i<nodes.size(); ++i){ // Stop x
+        for(int j=i+1; j<nodes.size(); ++j){ // Stop y
+            double distance12 = getDistance(nodes[i], nodes[j]); // Distance between x - y
+            if(distance12 < 1){
+                bool flag = false;
+                for(Edge e: nodes[i].adj){
+                    if(e.dest==j) flag = true; // if x already has connection to y by transport
+                }
+                if(flag) continue; // continue if condition true
+                
+                addEdge(i, j, distance12*2, "walk");
+            }
+        }
+    }
 }
 
 

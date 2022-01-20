d371c56a72dd503a526d17f4475c2f793f6536eb
jogp10 <jogp10@gmail.com>
2022-01-20 22:10:47 +0000

    Graph.h

--- develop/graph/graph.h
+++ develop/graph/graph.h
@@ -42,17 +42,53 @@ public:
     // Constructor: nr nodes and direction (default: undirected)
     explicit Graph(int nodes, bool dir = false);
 
-    // Add edge from source to destination with a certain weight
+    /**
+     * Add edge from source to destination with a certain weight
+     * @param src Origin Stop
+     * @param dest Destination Stop
+     * @param weight Distance between Origin and Destination
+     * @param line Line wich i can get from Origin to Destination
+     */
     void addEdge(int src, int dest, double weight = 1.00, string line = "");
 
+    /**
+     * Set Node's updated with Stop's info
+     * @param Name of Stop
+     * @param Code of Stop
+     * @param Zone of Stop
+     * @param Latitude of Stop
+     * @param Longitude of Stop
+     * @param index in Graph
+     */
     void setNode(string name, string code, string zone, double latitude, double longitude, int index);
 
+    /**
+     *
+     * @return map where key: code of stop, value: latitude, longitude
+     */
     map<int, pair<double, double>> getNodes();
 
-    // ----- Functions to implement in this class -----
+    /**
+     * 
+     * @param a
+     * @param b
+     * @return
+     */
     double dijkstra_distance(int a, int b);
+
+    /**
+     *
+     * @param Departure local
+     * @param Destination local
+     * @return path;
+     */
     list<int> dijkstra_path(int a, int b);
 
+    /**
+ * Create edges to wich you can walk, without need to take transport
+ * This type of edge has in maximum weight 2, because the distance beetween 2 points is 1 km.
+ * We duplicated the default weight to this type of edge
+ */
     void createWalkEdges();
 };
 

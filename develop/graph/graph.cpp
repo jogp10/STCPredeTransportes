4fdf4339ffc3aa98a8b723f845116d13c6dcf3e0
jogp10 <jogp10@gmail.com>
2022-01-20 19:55:40 +0000

    Prepared graph to hold data

--- develop/graph/graph.cpp
+++ develop/graph/graph.cpp
@@ -9,10 +9,9 @@ Graph::Graph(int num, bool dir) : n(num), hasDir(dir), nodes(num+1) {
 }
 
 // Add edge from source to destination with a certain weight
-void Graph::addEdge(int src, int dest, int weight) {
+void Graph::addEdge(int src, int dest, int weight, string line) {
     if (src<1 || src>n || dest<1 || dest>n) return;
-    nodes[src].adj.push_back({dest, weight});
-    if (!hasDir) nodes[dest].adj.push_back({src, weight});
+    nodes[src].adj.push_back({dest, weight, line});
 }
 
 
